11cc02b68ae484c24346d1eec66bb23eb71a57ac
Erkan ŞİRİN <erkansirin78@hotmail.com>
2019-10-19 18:52:13 +0300

    final project solution added

--- final_project/SparkHive/src/main/scala/com/vbo/SparkHiveExample.scala
+++ final_project/SparkHive/src/main/scala/com/vbo/SparkHiveExample.scala
@@ -18,7 +18,52 @@ Logger.getLogger("org").setLevel(Level.WARN)
 
   import spark.implicits._
 
-  val df = spark.sql("SELECT * FROM foodmart.department")
-  df.show()
+  // I just need orderitemproductid, orderitemsubtotal, orderstatus
+  val order_items_orders_df = spark.sql(
+    """
+      |select orderitemproductid, orderitemsubtotal, orderstatus from azhadoop.order_items oi
+      |inner JOIN azhadoop.orders o
+      |ON oi.orderitemorderid = o.orderid
+      |""".stripMargin)
+
+  println("order_items_orders_df:")
+  order_items_orders_df.show(5)
+
+
+  // I need productid, categoryname
+  val products_categories_df = spark.sql(
+    """
+      SELECT productid, categoryname from azhadoop.products p
+      INNER JOIN azhadoop.categories c
+      ON p.productcategoryid = c.categoryid
+      """.stripMargin)
+    println("products_categories_df:")
+  products_categories_df.show(5)
+
+
+  val all_df = order_items_orders_df.join(products_categories_df,
+    order_items_orders_df.col("orderitemproductid") === products_categories_df.col("productid"),
+  "left")
+
+  all_df.show()
+
+
+
+  println("all_df count", all_df.count()) // 172.198
+
+  val cancelled_df = all_df.filter("orderstatus = 'CANCELED' ")
+
+  val result_df = cancelled_df.groupBy("categoryname")
+    .agg(F.sum("orderitemsubtotal").as("total"))
+    .orderBy(F.desc("total"))
+
+  println("result_df")
+  result_df.show()
+
+  result_df
+    .write
+    .mode("overwrite")
+    .saveAsTable("azhadoop.most_cancelled_catg")
+
 
 }
